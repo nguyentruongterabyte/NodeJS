d5f095c9b2148f0a3047e6db08545904697922e2
Nguyễn Thái Trưởng <91006957+nguyentruongterabyte@users.noreply.github.com>
2023-11-10 03:57:57 +0000

    05Tut

--- DaveGray/05Tut/logEvents.js
+++ DaveGray/05Tut/logEvents.js
@@ -1,11 +1,15 @@
-const { format } = require( 'date-fns' );
+const { format, parseISO } = require( 'date-fns' );
 const { v4: uuid } = require( 'uuid' );
 const fs = require( 'fs' );
 const fsPromises = require( 'fs' ).promises;
 const path = require( 'path' );
 
-const logEvents = async ( message ) => {
-  const dateTime = `${ format( new Date(), 'yyyy-mm-dd HH:mm:ss' ) }`;
+const logEvents = async ( message, logName ) => {
+  const currentDate = new Date();
+
+  const dateTime = `${ format(
+    currentDate
+    , 'yyyyMMdd hh:mm:ss') }`;
   const logItem = `${ dateTime }\t${ uuid() }\t${ message }`;
   console.log( logItem );
   try { 
@@ -13,7 +17,7 @@ const logEvents = async ( message ) => {
     if ( !fs.existsSync( path.join( __dirname, 'logs' ) ) ) {
       await fsPromises.mkdir( path.join( __dirname, 'logs' ) );
     }
-    await fsPromises.appendFile( path.join( __dirname, 'logs', 'eventLog.txt' ), logItem + '\n' );
+    await fsPromises.appendFile( path.join( __dirname, 'logs', logName ), logItem + '\n' );
   } catch ( err ) {
     console.error( err );
   }
