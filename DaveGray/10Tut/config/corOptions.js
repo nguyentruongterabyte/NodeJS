7b849a77d069c26636cf0a47538163f3def92cf8
Nguyễn Thái Trưởng <91006957+nguyentruongterabyte@users.noreply.github.com>
2023-11-12 05:35:10 +0000

    10Tut

---
+++ DaveGray/10Tut/config/corOptions.js
@@ -0,0 +1,13 @@
+const whiteList = ['https://www.google.com.vn/', 'http://localhost:5500', 'http://localhost:3500']
+const corsOptions = {
+  origin: ( origin, callback ) => {
+    if ( whiteList.indexOf( origin ) !== -1 || !origin) {
+      callback( null, true )
+    } else {
+      callback(new Error('Not allowed by CORS' ))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+
+module.exports = corsOptions;
