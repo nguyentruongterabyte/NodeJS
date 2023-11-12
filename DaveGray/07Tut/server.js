161af5fc272797263bbd3047a0dada386bf37b62
Nguyễn Thái Trưởng <91006957+nguyentruongterabyte@users.noreply.github.com>
2023-11-12 02:02:54 +0000

    07Tut

--- DaveGray/07Tut/server.js
+++ DaveGray/07Tut/server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require( 'path' );
 const cors = require( 'cors' );
 const { logger } = require('./middleware/logEvents');
+const  errHandler  = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -12,7 +13,7 @@ app.use( logger );
 const whiteList = ['https://www.google.com.vn/', 'http://localhost:5500', 'http://localhost:3500']
 const corsOptions = {
   origin: ( origin, callback ) => {
-    if ( whiteList.indexOf( origin ) !== -1 ) {
+    if ( whiteList.indexOf( origin ) !== -1 || !origin) {
       callback( null, true )
     } else {
       callback(new Error('Not allowed by CORS' ))
@@ -73,9 +74,18 @@ const three = ( req, res, next ) => {
 
 app.get( '/chain(.html)?', [ one, two, three ] );
 
-app.get( '/*', ( req, res ) => {
-  res.status(404).sendFile( path.join( __dirname, 'views', '404.html' ) );
-} )
+app.all( '*', ( req, res ) => {
+  res.status( 404 );
+  if ( req.accepts( 'html' ) ){
+    res.sendFile( path.join( __dirname, 'views', '404.html' ) );
+  }
+  else if ( req.accepts( 'json' ) ) { 
+    res.json({error: "404 Not Found"});
+  } else {
+    res.type('txt').send("404 Not Found");
+  }
+} );
 
+app.use( errHandler );
 
 app.listen( PORT, () => console.log( `Server running on port ${PORT}` ) );
