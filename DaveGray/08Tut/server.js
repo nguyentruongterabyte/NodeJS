00a88457e3fa505ced5b380430f2880752c927f5
Nguyễn Thái Trưởng <91006957+nguyentruongterabyte@users.noreply.github.com>
2023-11-12 03:29:56 +0000

    08Tut

---
+++ DaveGray/08Tut/server.js
@@ -0,0 +1,56 @@
+const express = require( 'express' );
+const app = express();
+const path = require( 'path' );
+const cors = require( 'cors' );
+const { logger } = require('./middleware/logEvents');
+const  errHandler  = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use( logger );
+
+// Cross Origin Resource Sharing
+const whiteList = ['https://www.google.com.vn/', 'http://localhost:5500', 'http://localhost:3500']
+const corsOptions = {
+  origin: ( origin, callback ) => {
+    if ( whiteList.indexOf( origin ) !== -1 || !origin) {
+      callback( null, true )
+    } else {
+      callback(new Error('Not allowed by CORS' ))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+app.use( cors(corsOptions) );
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type': 'application/x-www-form-urlencoded'
+app.use( express.urlencoded( { extended: false } ) );
+
+// built-in middleware for json
+app.use( express.json() );
+
+// serve static files
+app.use( '/', express.static( path.join( __dirname, '/public' ) ) );
+app.use( '/subdir', express.static( path.join( __dirname, '/public' ) ) );
+
+app.use( '/', require( './routes/root' ) );
+app.use( '/subdir', require( './routes/subdir' ) );
+app.use( '/employees', require('./routes/api/employees' ) );
+
+app.all( '*', ( req, res ) => {
+  res.status( 404 );
+  if ( req.accepts( 'html' ) ){
+    res.sendFile( path.join( __dirname, 'views', '404.html' ) );
+  }
+  else if ( req.accepts( 'json' ) ) { 
+    res.json({error: "404 Not Found"});
+  } else {
+    res.type('txt').send("404 Not Found");
+  }
+} );
+
+app.use( errHandler );
+
+app.listen( PORT, () => console.log( `Server running on port ${PORT}` ) );
